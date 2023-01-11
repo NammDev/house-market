423f1e07db3e61f9be662d987fe542ba06bed96f
Nam <namdeveloper.ca@gmail.com>
2023-01-11 14:12:33 +0700

    Create Component Sign In & Sign Up

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,70 @@
+import { useState } from 'react'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import routes from '../config/routes'
+import { Link } from 'react-router-dom'
+
 function SignIn() {
-  return <h1>Sign In</h1>
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
+  const handleOnChange = (e) => {
+    setFormData((state) => ({
+      ...state,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <form>
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={handleOnChange}
+          />
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={handleOnChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt='show Password'
+              className='showPassword'
+              onClick={() => setShowPassword((state) => !state)}
+            />
+          </div>
+          <Link to={routes.forgotPassword} className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+        <Link to={routes.signUp} className='registerLink'>
+          Sign Up
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default SignIn
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,98 @@
+import { useState } from 'react'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import routes from '../config/routes'
+import { Link } from 'react-router-dom'
+
 function SignUp() {
-  return <h1>Sign Up</h1>
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+  const { name, email, password, confirmPassword } = formData
+
+  const handleOnChange = (e) => {
+    setFormData((state) => ({
+      ...state,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome to Market!</p>
+        </header>
+        <form>
+          <input
+            type='text'
+            className='nameInput'
+            placeholder='Username'
+            id='name'
+            value={name}
+            onChange={handleOnChange}
+          />
+          <input
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={handleOnChange}
+          />
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
+              value={password}
+              onChange={handleOnChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt='show Password'
+              className='showPassword'
+              onClick={() => setShowPassword((state) => !state)}
+            />
+          </div>
+          <div className='passwordInputDiv'>
+            <input
+              type={showConfirmPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Confirm Password'
+              id='confirmPassword'
+              value={confirmPassword}
+              onChange={handleOnChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt='show Password'
+              className='showPassword'
+              onClick={() => setShowConfirmPassword((state) => !state)}
+            />
+          </div>
+          <Link to={routes.forgotPassword} className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+          <div className='signUpBar'>
+            <p className='signUpText'>Sign Up</p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+        <Link to={routes.signIn} className='registerLink'>
+          Sign In
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default SignUp
