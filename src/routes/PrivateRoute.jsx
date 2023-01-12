7a4bd123b430462c58b96e270823cd49f1323763
Nam <namdeveloper.ca@gmail.com>
2023-01-13 05:37:36 +0700

    Check Curent Auth USer

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,16 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import routes from '../config/routes'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from '../components/Spinner'
 
-function PrivateRoute({ chilren }) {
-  const loggedIn = false
+function PrivateRoute({ children }) {
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
-  return loggedIn ? chilren : <Navigate to={routes.signIn} />
+  if (checkingStatus) {
+    return <Spinner />
+  }
+
+  return loggedIn ? children : <Navigate to={routes.signIn} />
 }
 
 export default PrivateRoute
